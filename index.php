60828f5027a5f19835db6bd6bef2529b8a422c1b
vanessamachado05 <machado.vanessasp@gmail.com>
2024-10-05 14:04:14 +0000

    adicionar primeira pagina php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá, mundo!</title>
+</head>
+<body>
+<?php
+  $numero1 = 25;
+  $numero2 = 60;
+
+  $soma = $numero1 + $numero2;
+?>
+<p>A soma de 25 e 60 é: <?php echo $soma ?> </p>
+</body>
+</html>

134f5d41300954621edee6c13306fbb0a892ef67
vanessamachado05 <machado.vanessasp@gmail.com>
2024-10-05 14:44:52 +0000

    adicionar calculadora php

---
+++ calculadora.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    
+   <h1>Calculadora fullstack!</h1>
+   <form action="calculadora.php" method="post">
+     
+    </form> 
+</body>
+</html>
